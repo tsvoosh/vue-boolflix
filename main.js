1791a42fd9acc4fdf08a27def25b1db401782559
Tsvetan <trendjazzer@gmail.com>
2021-04-07 16:17:17 +0200

    ok

--- main.js
+++ main.js
@@ -7,9 +7,42 @@ const app = new Vue({
 
     methods: {
         searchMovie() {
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=883ff48744295568b0bc7e5a825782e9', {params: {query : this.search}})
-                .then(response => this.results = response.data.results);
+            axios.get('https://api.themoviedb.org/3/search/multi', {
+                params: {
+                    api_key : '883ff48744295568b0bc7e5a825782e9',
+                    language: 'it',
+                    query : this.search,
+                }
+            })
+                .then((response) => {
+                    this.results = response.data.results.filter((person) => {
+                        return person.media_type != 'person'
+                    });
+                    console.log(this.results);
+                    this.results.forEach((result) => {
+                        result.vote_average = Math.ceil(result.vote_average / 2);
+                    })
+                });
             this.search = '';
+        },
+        setRating(rating, star) {
+            if (rating >= star) {
+                return 'fas fa-star'
+            } else {
+                return 'far fa-star'
+            }
+        },
+        getFlag(language) {
+            if (language == 'en') {
+                language = 'gb';
+            }
+            return "https://www.countryflags.io/" + language + "/shiny/64.png";
+        },
+        noImage(e) {
+            e.target.src = 'https://cdn.iconscout.com/icon/free/png-256/data-not-found-1965034-1662569.png';
+        },
+        getPoster(path) {
+            return 'http://image.tmdb.org/t/p/w342/' + path
         }
     }
 })
